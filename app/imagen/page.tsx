8f423db23edf7ca9d163f9e94cdd781c484cf7e4
Al <ali.al81@ui.ac.id>
2024-12-05 21:17:53 +0700

    Adde Dummy Imagen Page

---
+++ app/imagen/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import SearchPage from '@/components/search/SearchPage'
+import { Sidebar } from '@/components/Sidebar'
+import { HelpCircleIcon } from 'lucide-react'
+
+export default function Page() {
+  return (
+    <>
+      <Sidebar />
+      Hello, World!
+    </>
+  )}
