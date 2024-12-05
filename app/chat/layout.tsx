f8d73b943a91df8d0af4cd99d027263a11f8e88c
Al <ali.al81@ui.ac.id>
2024-12-05 22:52:06 +0700

    Fixed Metadata

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,18 +1,24 @@
-import { Images } from 'lucide-react'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'AI Chat | enaiblr',
+  title: 'AI Chat',
   description: 'Free AI Chat without Limits. No Login Required. Chat with Images.',
   openGraph: {
-    title: 'AI Chat | enaiblr',
+    title: 'AI Chat',
     description: 'Free AI Chat without Limits. No Login Required. Chat with Images.',
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'AI Chat | enaiblr',
+    title: 'AI Chat',
     description: 'Free AI Chat without Limits. No Login Required. Chat with Images.',
   }
 }
 
+export default function ChatLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
