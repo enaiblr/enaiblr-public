ef8617bab198a14d2689b5fd249d875596df71b2
Al <ali.al81@ui.ac.id>
2024-12-05 15:54:54 +0700

    Fixed Props

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,22 @@ import SearchPage from '@/components/search/SearchPage'
 import { Sidebar } from '@/components/Sidebar'
 import { Metadata } from 'next'
 
-type Props = {
-  params: { slug: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
 export async function generateMetadata({
   searchParams
-}: Props): Promise<Metadata> {
+}: {
+  searchParams: { q?: string }
+}): Promise<Metadata> {
   const query = searchParams?.q
   return {
     title: query ? `Search: ${query} | enaiblr` : 'enaiblr - AI Tools Search Engine',
   }
 }
 
-export default function Page({ searchParams }: Props) {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
   const query = typeof searchParams.q === 'string' ? searchParams.q : '';
   return (
     <>
